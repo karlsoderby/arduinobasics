f870a73eaf31434ae303ad5ddf2922f01d15600a
Karl Söderby <35461661+karlsoderby@users.noreply.github.com>
2018-07-19 10:34:39 +0200

    Photo resistor with LED
    This example will light an LED if the value of the photo resistor goes over 700.

---
+++ sunbeamish.ino
@@ -0,0 +1,23 @@
+int photoPin = A0;
+int ledPin = 2;
+int photoValue = 0;
+
+void setup() {
+  
+  pinMode(ledPin, OUTPUT);
+
+}
+
+void loop() {
+
+photoValue = analogRead(photoPin);
+Serial.println(photoValue);
+
+if(photoValue > 700) {
+digitalWrite(ledPin, HIGH);
+delay(3000);
+digitalWrite(ledPin, LOW);  
+}
+
+
+}
